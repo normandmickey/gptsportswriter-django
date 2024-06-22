a4f426541e280c9ffc170ec97b97317875c9e1c6
normandmickey <normandmickey@gmail.com>
2024-06-22 05:53:51 -0400

    added image generation

--- predictions/views.py
+++ predictions/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.timezone import datetime
 from .chat_completion import generate_prediction
+from .image_generation import generate_image
 
 
 # Create your views here.
@@ -22,10 +23,16 @@ def predictions(request):
             user_input += request.POST.get("text_input") + "\n"
 
         generated_prediction = generate_prediction(user_input)
+        image_prompt = (
+            f"Generate an image that visually illustrates the essence of the following story: {generated_prediction}"
+        )
+        image_url = generate_image(image_prompt)
+
         
         context = {
             "user_input": user_input,
             "generated_prediction": generated_prediction.replace("\n", "<br/>"),
+            "image_url": image_url,
         }
 
         return render(request, "predictions/predictions.html", context)
