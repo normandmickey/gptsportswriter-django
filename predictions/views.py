6779e37e30f48b91db6e51d3b5f8470af83bd6f2
normandmickey <normandmickey@gmail.com>
2024-06-22 07:21:18 -0400

    updated image path

--- predictions/views.py
+++ predictions/views.py
@@ -40,6 +40,7 @@ def predictions(request):
             f"Generate an image that visually illustrates the essence of the following story: {generated_prediction}"
         )
         image_url = generate_image(image_prompt)
+        print(image_url)
         data = requests.get(image_url).content
         f = open('img.jpg', 'wb')
         f.write(data)
