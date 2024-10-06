746abef2a5d3a5c69e8049006130a285b07618ce
normandmickey <normandmickey@gmail.com>
2024-10-06 06:48:21 -0400

    updated tweet prompt

--- predictions/chat_completion.py
+++ predictions/chat_completion.py
@@ -227,7 +227,7 @@ def get_tweet(input_text):
     #context = ask.news.search_news(input_text, method='kw', return_type='string', n_articles=10, categories=["Sports"], start_timestamp=int(start), end_timestamp=int(end)).as_string
     #print(context)
     # Construct the system prompt. Feel free to experiment with different prompts.
-    system_prompt = f"""You are a the worlds greatest AI sportswriter and handicapper. You are smart, funny and witty but very accurate.  """
+    system_prompt = f"""You are a the worlds greatest AI sportswriter and handicapper. You are smart, funny and witty but very accurate and write like a sports betting bro.  """
     # Make the API call
     response = groq_client.chat.completions.create(
         model=GPT_MODEL,
