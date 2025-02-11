bed7868fdb6e7485a215a476792721ff910f047f
normandmickey <normandmickey@gmail.com>
2025-02-11 05:01:06 -0500

    updated prompt

--- predictions/chat_completion.py
+++ predictions/chat_completion.py
@@ -53,7 +53,7 @@ def get_prediction(input_text, guaranteedWords, oddsJson):
             model=GPT_MODEL,
             messages=[
                 {"role": "system", "content": system_prompt},
-                {"role": "user", "content": "Write a humorous prediction for the following matchup.  Include only relevant stats and odds for the game in question note any injiries or significant players. Give your best bet based on the context provided take into account that underdogs win about 41 percent of the time in baseball and hockey, 35 percent in football and 25 percent in baskeball. Do not make up any details." + context + str(oddsJson) + " " + input_text},
+                {"role": "user", "content": "Write a humorous, sarcastic prediction for the following matchup.  Include only relevant stats and odds for the game in question note any injiries or significant players. Give your best bet based on the context provided take into account that underdogs win about 41 percent of the time in baseball and hockey, 35 percent in football and 25 percent in baskeball. Do not make up any details." + context + str(oddsJson) + " " + input_text},
             ],
             temperature=0.3, 
             max_tokens=1000
