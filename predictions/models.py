7be6eaee8127d031ad976a5537411e19263ddf90
normandmickey <normandmickey@gmail.com>
2025-05-23 22:20:13 -0400

    updated slug

--- predictions/models.py
+++ predictions/models.py
@@ -3,6 +3,26 @@ from django.utils import timezone
 from django.utils.text import slugify
 from django.urls import reverse
 import base64
+import random
+import string
+
+def generate_random_string(length=4):
+    """Generates a random string of given length."""
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+def generate_unique_slug(instance, slug_field, title, new_slug=None):
+    """Generates a unique slug for a given instance."""
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(title)
+
+    Klass = instance.__class__
+    if Klass.objects.filter(**{slug_field: slug}).exists():
+        new_slug = f"{slug}-{generate_random_string()}"
+        return generate_unique_slug(instance, slug_field, title, new_slug=new_slug)
+    return slug
 
 class Predictions(models.Model):
     id = models.TextField(primary_key=True)
@@ -14,7 +34,8 @@ class Predictions(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            #self.slug = slugify(self.title)
+            self.slug = generate_unique_slug(self, 'slug', self.title)
         super().save(*args, **kwargs)
     
     def get_absolute_url(self):
@@ -34,7 +55,8 @@ class Recaps(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            #self.slug = slugify(self.title)
+            self.slug = generate_unique_slug(self, 'slug', self.title)
         super().save(*args, **kwargs)
     
     def get_absolute_url(self):
@@ -54,7 +76,8 @@ class Parlays(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            #self.slug = slugify(self.title)
+            self.slug = generate_unique_slug(self, 'slug', self.title)
         super().save(*args, **kwargs)
     
     def get_absolute_url(self):
@@ -74,7 +97,8 @@ class Props(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            #self.slug = slugify(self.title)
+            self.slug = generate_unique_slug(self, 'slug', self.title)
         super().save(*args, **kwargs)
     
     def get_absolute_url(self):
